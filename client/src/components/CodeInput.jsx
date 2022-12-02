2d2b1f2beadf1dc7f0fe6aa78f3b5f9d1b0d4f26
mweide <ms22@gomediapro.com>
2022-12-02 10:04:29 -0700

    got codeinput and eventselector to pass input

--- client/src/components/CodeInput.jsx
+++ client/src/components/CodeInput.jsx
@@ -1,18 +1,36 @@
 import React from "react"
-import { useState } from "react"
-import Search from './Search'
+import { useState, useRef } from "react"
+import axios from "axios"
 
 const CodeInput = (props) => {
 
     const [code, setCode] = useState('')
+    const [columns, setColumns] = useState([])
+    const [results, setResults] = useState([])
     const searchType = props.searchType
+
+    const ref1 = useRef(null)
+
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
+    const search = () => {
+        axios.get(`${process.env.REACT_APP_HOST}/api/read/peopleByCodes/${code}`).then((response) => {
+            setResults(response.data)})
+        setColumns(["First Name", "Last Name", "Email Address", "Ticket Code"])
+    }
 
     return(
         <div id="code-input">
             <label>
-                <input name="codeInput" id="codeInput" type="text" onChange={e => setCode(e.target.value)}></input>
+                <input ref={ref1} name="codeInput" id="codeInput" type="text" onChange={e => setCode(e.target.value)}></input>
             </label>
-            <Search searchType={searchType} codeValue={code} />
+            <button id="search-button" onClick={() => {
+            if (code.length > 0) {
+              search(); refreshPage();
+            }
+          }}>Search</button>
         </div>
     )
 }
--- client/src/components/EventSelector.jsx
+++ client/src/components/EventSelector.jsx
@@ -1,16 +1,32 @@
 import React from "react"
 import { useState } from "react"
 import Select from "react-select"
-import Search from './Search'
+import axios from "axios"
 
 const EventSelector = (props) => {
     const [selectedEvent, setSelectedEvent] = useState('default')
-    const options = ["1","2","3","4"] //create query for events list
+    const options = [{value:"1", label:"1"},{value:"2", label:"2"},{value:"3", label:"3"},{value:"4", label:"4"}] //create query for events list
 
     const searchType = props.selectedValue
-    const handleChange = e => {
-        e.preventDefault()
-        setSelectedEvent(e.value)
+
+    const [results, setResults] = useState([])
+    const [columns, setColumns] = useState([])
+
+    function refreshPage() {
+        window.location.reload(false);
+    }
+
+    const search = () => {
+        if(searchType === "codesByEvent"){
+            axios.get(`${process.env.REACT_APP_HOST}/api/read/codesByEvent/${selectedEvent}`).then((response) => {
+                setResults(response.data)})
+            setColumns(["Event Name", "Event Date", "Ticket Code"])
+        }
+        if(searchType === "peopleByEvents"){
+            axios.get(`${process.env.REACT_APP_HOST}/api/read/peopleByEvents/${selectedEvent}`).then((response) => {
+                setResults(response.data)})
+            setColumns(["Event Name", "Event Date", "First Name", "Last Name"])
+        }
     }
 
     return(
@@ -20,11 +36,15 @@ const EventSelector = (props) => {
                 id="event-selector" 
                 options={options}
                 placeholder="Select event"
-                value = {selectedEvent}
-                onChange={handleChange} />
+                value = {options.find(obj => obj.value === selectedEvent)}
+                onChange={e=> {setSelectedEvent(e.value)}} />
             </form>
             <div>
-                <Search searchType={searchType} selectedEvent={selectedEvent}/>
+            <button id="search-button" onClick={() => {
+            if (selectedEvent.length > 0) {
+              search(); refreshPage();
+            }
+          }}>Search</button>
             </div>
         </div>
     )
