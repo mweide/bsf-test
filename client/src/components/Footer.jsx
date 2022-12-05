b58b8004f38f0827ecd1b029b4c6f9b4b255b304
mweide <ms22@gomediapro.com>
2022-12-04 21:35:13 -0700

    Updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,13 +4,18 @@ const Footer = () => {
         <div>
             <h2>Footer</h2>
             <p><em> Megan Weide ~ r94r698</em></p>
-            <p>My role in the project was to build the app.</p>
+            <p>My role in the project was to build a search functionality and to style the app.</p>
             <ul>
-                <li>test</li>
-                <li>client/App.css -  basic styling for form and volunteer list </li>
-                <li>client/AddEntry.jsx - A react component to collect volunteer data</li>
-                <li>client/CurrentEntries.jsx - React compoment listing volunteers to be emailed</li>
-                <li>server/index.js - an Express server connecting MySQL with React frontend</li>
+                <li>Updated components/AddEntry.jsx with styling</li>
+                <li>Updated components/CurrentEntries.jsx with styling</li>
+                <li>components/CodeInput.jsx - A component to provide a place to input code in code searches and to initiate those searches</li>
+                <li>components/EventSelector.jsx - A component to provide a place to select an event for event searches and to initiate those searches</li>
+                <li>components/PeopleInput.jsx - A component to provide a place to input first/last names for people-based searches and to initiate those searches</li>
+                <li>components/Selector.jsx - The tool to select the search type to conduct</li>
+                <li>components/SetInputs.jsx - A component to set the proper inputs for the given search</li>
+                <li>Updated components/App/App.css with some styling</li>
+                <li>Updated components/App/App.js to include the main component of our feature, the selector</li>
+                <li>Updated server/index.js with paths for searches to take to query the database</li>
             </ul>
             <p>Github: <a href="https://github.com/dldf/BSF-starter">https://github.com/dldf/BSF-starter</a></p>
         </div>
